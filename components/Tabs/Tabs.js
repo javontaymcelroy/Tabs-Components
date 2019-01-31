af5be464e49ecfd1ba0d1010f207a848888818f0
Javontay McElroy <dinomightnetwork@gmail.com>
2019-01-31 16:26:22 -0600

    I am confused...

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -4,33 +4,34 @@ class TabLink {
     this.element = document.querySelector(`.tabs-items[data-tab='${element.dataset.tab}']`);
     // Assign this.element to the passed in DOM element
     // this.element;
-    this.data =  
+    this.data =  document.querySelector(`.tabs-items[data-tab='${element.dataset.tab}']`);  
     // Get the custom data attribute on the Link
     // this.data;
-    
+    this.itemElement = document.querySelector(`.tabs-item[data-tab='${element.dataset.tab}']`);
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
     
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
-    this.element = new TabItem(this.element);
+    this.tabItem = new TabItem(this.itemElement);
     // Add a click event listener on this instance, calling the select method on click
-    element.addEventListener('click', () => this.select() );
+    this.element.addEventListener('click', () => this.select());
 
   };
 
   select() {
+    const links = document.querySelectorAll('.tabs-link');
     // Get all of the elements with the tabs-link class
     // const links;
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
-
+    
     // Add a class named "tabs-link-selected" to this link
     // this.element;
-    
+    this.element.classList.add('tabs-link-selected');
     // Call the select method on the item associated with this link
-
+    this.tabItem.select();
   }
 }
 
@@ -38,16 +39,18 @@ class TabItem {
   constructor(element) {
     // Assign this.element to the passed in element
     // this.element;
+    this.element = element;
   }
 
   select() {
     // Select all ".tabs-item" elements from the DOM
     // const items;
-
+    const items = document.querySelectorAll('.tabs-item');
     // Remove the class "tabs-item-selected" from each element
     
     // Add a class named "tabs-item-selected" to this element
     //this.element;
+    this.element.classList.add('tabs-item-selected')
   }
 }
 
